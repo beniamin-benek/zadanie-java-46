163badfe0fa0462d378c4c2352fc2c1ae3b61a6d
benek <ben.lyzn@wp.pl>
2019-01-22 22:59:22 +0100

    Rozwiązanie.

---
+++ zadanie-java-46/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println(Character.toString(75) + Character.toString(79) + Character.toString(84));
+        //https://en.wikipedia.org/wiki/List_of_Unicode_characters
+
+    }
+
+}
